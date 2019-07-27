fefe2255a8f28ad9dd9f57f78afe81bece3b49a4
amaradevi <amaradevi@gmail.com>
2019-07-27 07:13:42 +0530

    test changes

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM ubuntu:16.04
+
+
+RUN apt-get update -y && \
+    apt-get install -y python-pip python-dev
+
+# We copy just the requirements.txt first to leverage Docker cache
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+ENV FLASK_APP=webapp.py
+
+CMD sleep 10000
+
+
+
+
+
+
---
+++ webapp.py
@@ -0,0 +1,44 @@
+from flask import Flask,jsonify,request,json
+import os,sys
+import time
+app = Flask(__name__)
+
+employees = [{"id":1,"fname":"Amara","lname":"Devi","Dep":"OMC"},
+	{"id":2,"fname":"Kamal","lname":"Singh","Dep":"OMC"}
+]
+
+
+@app.route("/",methods=['GET'])
+def get_employees():
+	return jsonify(employees)
+
+@app.route("/",methods=['POST'])
+def create_employee():
+	employee={"id":employees[-1]["id"]+1,"fname":request.json["fname"],"lname":request.json["lname"],"Dep":request.json["dep"]}
+	employees.append(employee)
+	return jsonify({"json created":employee}), 201
+
+@app.route("/employee/<int:empnum>",methods=['GET'])
+def get_employee(empnum):
+		
+	employee=[e for e in employees if e["id"]==empnum]
+	if len(employee) == 0:
+		return jsonify({"Employee not exists":empnum}), 404
+	return jsonify(employee)
+	
+@app.route("/employee/<int:empnum>",methods=['DELETE'])
+def delete_employee(empnum):
+		
+	employee=[e for e in employees if e["id"]==empnum]
+	if len(employee) == 0:
+		return jsonify({"Employee not exists":empnum}), 404
+	employees.remove(employee[0])
+	return jsonify({"employee deleted":empnum}), 204
+		
+
+	
+	
+if __name__=='__main__':
+	app.run(debug=True, host='0.0.0.0')
+	
+	
